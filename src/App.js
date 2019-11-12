57355cc24f21bc0fbd9432f9ca1cf2b29ad1e304
YONGHAPAPA <zerobreaker@gmail.com>
2019-11-12 18:20:55 +0900

    20191112

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {subscribeToTimer} from './api';
+import { tsConstructorType } from '@babel/types';
+
+
+
+class App extends Component {
+   constructor(props){
+    super(props);
+
+    this.state = {
+      timestamp : 'no timestamp yet.'
+    }
+
+    subscribeToTimer((err, timestamp) => this.setState({
+      timestamp
+    }));
+
+   }
+
+   render(){
+      return (
+        <div className="App">
+          <p className="App-intro">
+            {this.state.timestamp}
+          </p> 
+        </div>
+      );
+    }
 }
 
 export default App;
